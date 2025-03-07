299d7592e2d2c707a935824be2229044608ebc26
Shrishail  Bidave <167215447+Shri1927@users.noreply.github.com>
2025-03-07 23:52:36 +0530

    problem

---
+++ missingnumber.java
@@ -0,0 +1,22 @@
+public class missingnumber {
+    
+    static int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int expectedSum = n*(n+1)/2;
+        int actualSum = 0;
+
+        for(int i=0; i<n; i++){
+            actualSum += nums[i];
+        }
+
+        return expectedSum-actualSum;
+        
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {0,1,3};
+        int res = missingNumber(arr);
+        System.out.println(res);
+    }
+}
